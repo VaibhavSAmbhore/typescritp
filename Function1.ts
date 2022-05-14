6bbbe278a29cd6dbce5b7ca2c3bc403cd2ba1fef
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-05-14 23:57:37 +0530

    Add files via upload

---
+++ Function1.ts
@@ -0,0 +1,18 @@
+function fun():void//Function defination
+{
+  console.log("inside fun");
+}
+
+fun();//function call
+
+
+/*
+
+void fun()
+{
+  printf("inside fun");
+  cout<<"inside fun";
+  System.out.println("inside fun");
+}
+*/
+// syntax is way of writing the code
