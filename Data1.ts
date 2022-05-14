b731120b3871fea5628bda3e1fbe6a59bbbc3c11
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-05-14 23:57:10 +0530

    Add files via upload

---
+++ Data1.ts
@@ -0,0 +1,8 @@
+//int no = 11; c/c++/c#/java
+
+
+var a1=11;//js ts
+var a2:number=11;//ts
+console.log(a1);
+console.log(a2);
+//number->int,Float,double,short,long
---
+++ Data2.js
@@ -0,0 +1,6 @@
+var Str1 = "Hello";
+console.log(Str1);
+console.log(typeof Str1);
+var no = 11;
+console.log(no);
+console.log(typeof no);
