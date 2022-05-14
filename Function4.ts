6463f24ecec21a546d6eed15b2c7bb452c8f18b2
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-05-14 23:58:37 +0530

    Add files via upload

---
+++ Function4.ts
@@ -0,0 +1,15 @@
+function main():void  //function defination
+{
+ console.log("inside main");
+ fun();
+ console.log("End of main");
+
+}
+
+function fun():void //function defination
+{
+  console.log("inside fun");
+}
+main(); // function call
+
+
