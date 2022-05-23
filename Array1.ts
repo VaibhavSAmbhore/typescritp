d4909cba79516b6cc31ac270b80bbf05243b0a91
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-05-23 22:57:32 +0530

    Add files via upload

---
+++ Array1.ts
@@ -0,0 +1,18 @@
+// int Array[5]={10,20,30,40,50}; c c++ java C#
+
+//Arr is one dimentional array which contain  5 element each element id of type number.
+
+var Arr:number[]=[10,20,30,40,50,];
+
+console.log("Length of array is:"+Arr.length);
+console.log("first element is"+Arr[0]);
+console.log("second element is:"+Arr[1]);
+
+console.log("Elements from array ara:");
+var i:number=0;
+
+for(i=0;i<Arr.length;i++)
+{
+  console.log(Arr[i]);
+}
+
